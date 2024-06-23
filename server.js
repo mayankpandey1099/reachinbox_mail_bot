5af429869a3d84b8d5d5e81bf722c3fabb30fbae
mayank pandey <mayankpandey1099@gmail.com>
2024-06-23 11:23:36 +0530

    added files for controller, util, router, middleware

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 require("dotenv").config();
 
 const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
 
 const app = express();
+
+
+app.use(cors());
+app.use(
+  session({
+    secret: "session",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 
 const port = process.env.PORT || 3000;
 
